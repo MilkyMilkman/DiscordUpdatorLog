517ba38139276d966bdced89a18e62ead5feca50
Thot-Hunter <48199659+Thot-Hunter@users.noreply.github.com>
2019-03-16 15:27:54 +0700

    Update Program.cs

--- DiscordUpdateLogs/Program.cs
+++ DiscordUpdateLogs/Program.cs
@@ -14,7 +14,7 @@ namespace DiscordUpdateLogs
         {
             Process Discord = new Process();
 
-            Discord.StartInfo.FileName = @"C:/Users/(your_computer_name)/AppData/Local/Discord/app-0.0.304/Discord.exe";
+            Discord.StartInfo.FileName = @"C:/Users/(your_windows_username)/AppData/Local/Discord/app-0.0.305/Discord.exe";
 
             Discord.Start();
 
