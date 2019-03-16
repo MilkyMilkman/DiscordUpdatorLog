883d09341fb8f0ae75f0e76f56e17096ebf02e82
Thot-Hunter <48199659+Thot-Hunter@users.noreply.github.com>
2019-03-16 15:14:17 +0700

    Add files via upload
    Not sure if tons of people know about this, but i just found this out by accident. There's no need to download any Libraries, almost everything is there when you create a C# project, you just need to add a System.Diagnostics
    
    Anyways, give me some feedbacks

---
+++ DiscordUpdateLogs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace DiscordUpdateLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process Discord = new Process();
+
+            Discord.StartInfo.FileName = @"C:/Users/(your_computer_name)/AppData/Local/Discord/app-0.0.304/Discord.exe";
+
+            Discord.Start();
+
+            Console.Read();
+        }
+    }
+}
